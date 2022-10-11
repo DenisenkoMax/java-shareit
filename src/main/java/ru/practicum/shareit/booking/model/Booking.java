ba07134921365e9ef1f8e6edaa8b71d57fbfb634
Max <denmax@mail.ru>
2022-10-12 01:00:41 +0700

    Спринт 15

--- src/main/java/ru/practicum/shareit/booking/model/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.booking.model;
 import lombok.*;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
-
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
