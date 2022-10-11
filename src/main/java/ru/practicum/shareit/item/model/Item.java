e8b7618cbc11cd2d99191a7e6b9607fc29e788bd
Max <denmax@mail.ru>
2022-10-11 19:14:16 +0700

    Спринт 15

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -54,4 +54,9 @@ public class Item implements Serializable {
         Item item = (Item) o;
         return id != null && Objects.equals(id, item.id);
     }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
