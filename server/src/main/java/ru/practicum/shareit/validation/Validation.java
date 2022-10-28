af9c04733d21af0c55056bf75881be375802d18a
Max <denmax@mail.ru>
2022-10-28 14:52:35 +0700

    Спринт 16 После ревью 1. Перенес проверки из server в gateway. Добавил комментарии (для себя) к валидации, требующей работы с БД

--- server/src/main/java/ru/practicum/shareit/validation/Validation.java
+++ server/src/main/java/ru/practicum/shareit/validation/Validation.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.validation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.booking.BookingRepositoryJpa;
-import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.exception.IllegalArgumentEx;
 import ru.practicum.shareit.exception.NotFoundEx;
